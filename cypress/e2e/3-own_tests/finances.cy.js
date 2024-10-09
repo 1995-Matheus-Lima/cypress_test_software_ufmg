6072e2a526f2a2361449ed7b0e625ace12b36d2d
Matheus Henrique <1995.matheus.lima@gmail.com>
2024-10-09 10:19:37 -0300

    Final changes in Seminary code project

--- cypress/e2e/3-own_tests/finances.cy.js
+++ cypress/e2e/3-own_tests/finances.cy.js
@@ -1,5 +1,5 @@
 
-const criarTransação = (description, valor, date) => {
+const criarTransacao = (description, valor, date) => {
   /* Seleciona o elemento que contem o texto Nova Transação e clica nele */
   cy.contains("Nova Transação").click()
   // Seleciona o Elemento com id Descripton e insere um texto
@@ -13,9 +13,28 @@ const criarTransação = (description, valor, date) => {
   //cy.contains('button', 'Salvar').click()
 
   //Acessa a tabela renderizada e ver se nela existe um elemento com texto freela
-  cy.get("tbody tr td.description").should("have.text", description)
+  cy.get("tbody tr td.description").should("contain.text", description)
 }
 
+const excluirTransacao = (description, value) =>  {
+  //Cria transações
+  criarTransacao(description,value,"2024-10-08")
+  criarTransacao("Freela",500, "2024-10-08")
+
+  //Seleciona somente a transação em description
+  // cy.contains(".description",description)
+  //   .parent() //Seleciona elemento pai
+  //   .find('img') // seleciona o elemento img dentro do site
+  //   .click() // clica no elemento
+
+  cy.contains(".description",description)
+    .siblings()
+    .find('img')
+    .click()
+  
+  //checa de se número de linhas na tabela final é igual a 1
+  cy.get('tbody tr').should("have.length", 1)
+}
 
 describe('Transações', () =>{
 
@@ -23,11 +42,14 @@ describe('Transações', () =>{
     cy.visit("https://devfinance-agilizei.netlify.app/#")
   }) 
 
-  it('Cadstrar uma entrada', () =>{
-    criarTransação("Freela",500, "2024-10-08")
+  it('Cadastrar uma entrada', () =>{
+    criarTransacao("Freela",500, "2024-10-08")
   })
-  it('Cadstrar uma Saida', () =>{
-    criarTransação("cinema",-60, "2024-10-04")
+  it('Cadastrar uma Saida', () =>{
+    criarTransacao("cinema",-60, "2024-10-04")
+  })
+  it("Excluir uma transação cadastrada", () =>{
+    excluirTransacao("pão de queijo", -300)
   })
 })
 
